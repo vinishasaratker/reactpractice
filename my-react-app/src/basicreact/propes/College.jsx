a2f953b9e242e59e169af6d97671f10a3a42b7e0
vinishasaratker <vinishasaratker@gmail.com>
2026-02-03 16:43:30 +0530

    hy

--- my-react-app/src/basicreact/propes/College.jsx
+++ my-react-app/src/basicreact/propes/College.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const College = (propes) => {
   return (
     <div>
-      <h1>my college name is : {propes.name}</h1>
+      <h1>My College Name is : {propes.name}</h1>
     </div>
   )
 }
