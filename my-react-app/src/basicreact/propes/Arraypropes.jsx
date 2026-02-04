c7045f752a8e0f3dfdc019b020763512ba059e6d
vinishasaratker <vinishasaratker@gmail.com>
2026-02-04 16:29:55 +0530

    hy

--- my-react-app/src/basicreact/propes/Arraypropes.jsx
+++ my-react-app/src/basicreact/propes/Arraypropes.jsx
@@ -4,7 +4,9 @@ const Arraypropes = (props) => {
   return (
     <div>
       Roll No: {props.rno} <br/>
+
       Name: {props.name} <br/>
+      
       Fees: {props.fs}
       <hr/>
     </div>
--- my-react-app/src/basicreact/propes/Parentarraypropes.jsx
+++ my-react-app/src/basicreact/propes/Parentarraypropes.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import Arraypropes from "./Arraypropes";
+import React from 'react';
 
 const Student = [
   { rollno:101, name:"mahima", fees:766 },
   { rollno:102, name:"moti", fees:664 },
-  { rollno:104, name:"hurry", fees:885 },
+  { rollno:104, name:"HENRY", fees:885 },
   { rollno:106, name:"arti", fees:9999 }
 ];
 
 const Parentarraypropes = () => {
   return (
-    <div>
-      {
-      Student.map(stu => (
-        <Arraypropes
-          key={stu.rollno}
-          rno={stu.rollno}
-          name={stu.name}
-          fs={stu.fees}
-        />
-      ))}
-    </div>
-  )
-}
+    <table border="2">
+      
+        <tr>
+          <th>Roll No</th>
+          <th>Name</th>
+          <th>Fees</th>
+        </tr>
+    
+      
+        {Student.map((stu) => (
+          <tr key={stu.rollno}>
+            <td>{stu.rollno}</td>
+            <td>{stu.name}</td>
+            <td>{stu.fees}</td>
+          </tr>
+        ))}
+     
+    </table>
+  );
+};
 
 export default Parentarraypropes;
